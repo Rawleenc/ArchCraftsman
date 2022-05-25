999945e6214716c9806df562ef5e231e36491377
Rawleenc <lawrence.dnp@gmail.com>
2022-05-25 22:53:50 +0200

    Update reflector command

--- archlinux_install.py
+++ archlinux_install.py
@@ -1002,7 +1002,7 @@ def main(pre_launch_info):
                              partitioning_info["part_format"].get(partition))
 
     print_step(_("Updating mirrors..."), clear=False)
-    os.system('reflector --verbose -phttps -f10 -l10 --sort rate -a2 --save /etc/pacman.d/mirrorlist')
+    os.system('reflector --verbose -phttps -f10 -l10 --sort rate -a12 --save /etc/pacman.d/mirrorlist')
 
     base_pkgs = set()
     base_pkgs.update(["base", "base-devel", "linux-firmware"])
