fa4b06b9d13982f3b84fb587e8f42f53625cdbe6
Rawleenc <lawrence.dnp@gmail.com>
2022-10-27 22:27:06 +0200

    Add noto-fonts-emoji to main fonts bundle

--- archcraftsman.py
+++ archcraftsman.py
@@ -609,7 +609,7 @@ def get_main_fonts() -> [str]:
     return ["gnu-free-fonts", "noto-fonts", "ttf-bitstream-vera", "ttf-dejavu", "ttf-hack", "ttf-droid",
             "ttf-fira-code", "ttf-fira-mono", "ttf-fira-sans", "ttf-font-awesome", "ttf-inconsolata",
             "ttf-input", "ttf-liberation", "ttf-nerd-fonts-symbols-2048-em", "ttf-opensans", "ttf-roboto",
-            "ttf-roboto-mono", "ttf-ubuntu-font-family", "ttf-jetbrains-mono", "otf-font-awesome"]
+            "ttf-roboto-mono", "ttf-ubuntu-font-family", "ttf-jetbrains-mono", "otf-font-awesome", "noto-fonts-emoji"]
 
 
 class MainFonts(Bundle):
