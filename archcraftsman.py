130bca4974a41ae9e9a5f5740cd14b7285496ceb
Rawleenc <lawrence.dnp@gmail.com>
2022-10-30 14:24:26 +0100

    Change budgie bundle to not install full gnome and install arc theme

--- archcraftsman.py
+++ archcraftsman.py
@@ -362,19 +362,28 @@ class Budgie(Bundle):
     """
     Bundle class.
     """
+    display_manager = True
 
     def packages(self, system_info) -> [str]:
         packages = ["budgie-desktop", "budgie-desktop-view", "budgie-screensaver", "gnome-control-center",
-                    "network-manager-applet", "gnome", "xorg-server", "alsa-utils", "pulseaudio",
-                    "pulseaudio-alsa", "pavucontrol"]
+                    "network-manager-applet", "gnome-terminal", "nautilus", "xorg-server", "alsa-utils", "pulseaudio",
+                    "pulseaudio-alsa", "pavucontrol", "arc-gtk-theme", "arc-icon-theme"]
+        if self.display_manager:
+            packages.extend(["lightdm", "lightdm-gtk-greeter", "lightdm-gtk-greeter-settings"])
         return packages
+
+    def prompt_extra(self):
+        self.display_manager = prompt_bool(
+            _("The display manager to install is '%s'. Do you want to install it ? (Y/n) : ") % "LightDM",
+            default=True)
 
     def print_resume(self):
         print_sub_step(_("Desktop environment : %s") % self.name)
-        print_sub_step(_("Display manager : %s") % "GDM")
+        print_sub_step(_("Display manager : %s") % "LightDM" if self.display_manager else _("none"))
 
     def configure(self, system_info, pre_launch_info, partitioning_info):
-        os.system('arch-chroot /mnt bash -c "systemctl enable gdm"')
+        if self.display_manager:
+            os.system('arch-chroot /mnt bash -c "systemctl enable lightdm"')
         os.system('arch-chroot /mnt bash -c "amixer sset Master unmute"')
         if "fr" in pre_launch_info["keymap"]:
             setup_chroot_keyboard("fr")
