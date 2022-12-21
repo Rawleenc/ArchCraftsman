13240ea5fe421797cf77db6f4d50cbce8daddb38
Rawleenc <lawrence.dnp@gmail.com>
2022-12-21 22:05:52 +0100

    Release 3.4.3

--- launcher.py
+++ launcher.py
@@ -8,7 +8,7 @@ import sys
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from urllib.request import urlretrieve
 
-REPO_BASE_URL = "https://raw.githubusercontent.com/rawleenc/ArchCraftsman/dev"
+REPO_BASE_URL = "https://raw.githubusercontent.com/rawleenc/ArchCraftsman/main"
 CMD = 'python -m src.archcraftsman'
 
 
