6c92b4e034560aa9ffb52da2379a158650acb29f
Rawleenc <lawrence.dnp@gmail.com>
2023-01-22 17:26:47 +0100

    Remove test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description="The ArchCraftsman installer.")
-parser.add_argument('-t', '--test', action='store_const', const=True, default=False,
-                    help='Used to test the installer. No command will be executed.')
-args = parser.parse_args()
-print(args)
-print(args.test)
