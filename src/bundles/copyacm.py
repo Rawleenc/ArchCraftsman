f089b6e7f0a748f346702ac94bdad4b272183004
Rawleenc <lawrence.dnp@gmail.com>
2023-01-22 15:48:11 +0100

    Remove useless import and f-strings

--- src/bundles/copyacm.py
+++ src/bundles/copyacm.py
@@ -3,7 +3,7 @@ The zram bundle module
 """
 from src.bundles.bundle import Bundle
 from src.i18n import I18n
-from src.utils import print_sub_step, log, execute
+from src.utils import print_sub_step, execute
 
 _ = I18n().gettext
 
@@ -22,6 +22,6 @@ class CopyACM(Bundle):
             execute(f'cp -r ~/src /mnt/home/{system_info["user_name"]}/ArchCraftsman')
             execute(f'cp -r ~/locales /mnt/home/{system_info["user_name"]}/ArchCraftsman')
         else:
-            execute(f'mkdir -p /mnt/root/ArchCraftsman')
-            execute(f'cp -r ~/src /mnt/root/ArchCraftsman')
-            execute(f'cp -r ~/locales /mnt/root/ArchCraftsman')
+            execute("mkdir -p /mnt/root/ArchCraftsman")
+            execute("cp -r ~/src /mnt/root/ArchCraftsman")
+            execute("cp -r ~/locales /mnt/root/ArchCraftsman")
