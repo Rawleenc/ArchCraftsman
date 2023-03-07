dce39fc4e3e05df3fcc838d328e378c5e67a0c76
Rawleenc <lawrence.dnp@gmail.com>
2023-03-07 23:22:58 +0100

    Release 3.5.3

--- launcher.py
+++ launcher.py
@@ -11,7 +11,7 @@ from urllib.request import urlretrieve, urlopen
 
 OWNER = "Rawleenc"
 REPO = "ArchCraftsman"
-BRANCH = "dev"
+BRANCH = "main"
 CMD = 'python -m src.archcraftsman --install'
 GREEN = "\033[0;32m"
 CYAN = "\033[0;36m"
